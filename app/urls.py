0558e26d7d9441350d85a0f81c53a8c2c4fe5f3c
Nilson Barboza <barbosa.nilsonneto@gmail.com>
2025-05-05 22:30:03 -0300

    Fix: Estrutura somente REST

--- app/urls.py
+++ app/urls.py
@@ -2,16 +2,13 @@ from django.urls import path
 from .views import register_user, user_profile
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from django.urls import path
-from .views import home_view, login_view,dashboard_view,logout_view
+
 
 urlpatterns = [
     path('register/', register_user, name='register_user'),
     path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('profile/', user_profile, name='user_profile'),
-    path('', home_view, name='home'),
-    path('login2/', login_view, name='login'),
-    path('dashboard/', dashboard_view, name='dashboard'),
-    path('logout/', logout_view, name='logout')
+    
     ]
 
--- app/views.py
+++ app/views.py
@@ -3,19 +3,7 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view,  permission_classes
 from .serializers import UserSerializer
 from rest_framework.permissions import IsAuthenticated
-from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login
-from django.contrib.auth.decorators import login_required
-from django.contrib.auth import logout as django_logout
 
-
-@login_required
-def dashboard_view(request):
-    return render(request, 'app/dashboard.html')
-
-def logout_view(request):
-    django_logout(request)
-    return redirect('home')
 
 
 @api_view(['POST'])
@@ -31,19 +19,3 @@ def register_user(request):
 def user_profile(request):
     serializer = UserSerializer(request.user)
     return Response(serializer.data)
-
-
-def home_view(request):
-    return render(request, 'app/home.html')
-
-def login_view(request):
-    if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
-        user = authenticate(request, username=username, password=password)
-        if user:
-            login(request, user)
-            return redirect('dashboard')
-        else:
-            return render(request, 'app/login.html', {'error': 'Credenciais inválidas.'})
-    return render(request, 'app/login.html')
